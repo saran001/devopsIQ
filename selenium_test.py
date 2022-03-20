d79cc7a1db7cba71225efb7d745fc06f3647b959
Saravanan <sarsky.murthy38@gmail.com>
2022-03-20 12:32:56 +0530

    Updating Test to Check all Deployed Containers.

--- selenium_test.py
+++ selenium_test.py
@@ -8,19 +8,20 @@ chrome_options = Options()
 chrome_options.add_argument("--headless")  
 chrome_options.binary_location = '/usr/bin/google-chrome'
 
-url="http://"+sys.argv[1]+":82/devopsIQ/";
-print("Testing Website: "+url)
-driver = webdriver.Chrome(executable_path=os.path.abspath("/usr/local/bin/chromedriver"), chrome_options=chrome_options)  
-driver.get(url);
-expectedTitle = "Cloudtrain Website";
-actualTitle = driver.title;
-print(actualTitle);
-if actualTitle == expectedTitle:
-    print("Website Title Expected: "+ expectedTitle);
-    print("Website Title Actual: "+ actualTitle);
-    print("TEST PASSED!");
-else:
-    print("Website Title Expected: "+ expectedTitle);
-    print("Website Title Actual: "+ actualTitle);
-    print("TEST FAILED!");
+for i in range(81,86):
+    url="http://"+sys.argv[1]+":" + str(i) + "/devopsIQ/";
+    print("Testing Website: "+url)
+    driver = webdriver.Chrome(executable_path=os.path.abspath("/usr/local/bin/chromedriver"), chrome_options=chrome_options)  
+    driver.get(url);
+    expectedTitle = "Cloudtrain Website";
+    actualTitle = driver.title;
+    print(actualTitle);
+    if actualTitle == expectedTitle:
+        print("Website Title Expected: "+ expectedTitle);
+        print("Website Title Actual: "+ actualTitle);
+        print("TEST PASSED!");
+    else:
+        print("Website Title Expected: "+ expectedTitle);
+        print("Website Title Actual: "+ actualTitle);
+        print("TEST FAILED!");
 driver.close()
